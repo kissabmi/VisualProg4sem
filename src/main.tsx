015271d13c0f2b987faf5c6d0c17f0ce6655c987
GlebChernyshev <123za123g@gmail.com>
2026-04-02 10:37:34 +0700

    part 1 and 2 completed

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,14 @@
-//empty
+import react from 'react';
+import reactDom from 'react-dom/client';
+import App from './App';
+
+//ищем div с id ="root"
+const rootElement = document.getElementById('root') as HTMLElement;
+
+// в этом div-е мы будем рендерить наше приложение
+const root = reactDom.createRoot(rootElement);
+root.render(
+    <react.StrictMode>
+        <App />
+    </react.StrictMode>
+);
